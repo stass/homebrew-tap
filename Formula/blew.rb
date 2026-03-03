12f35d4f6e3b3b4e8887e653ae6e7b7eaf3fe812
Stanislav Sedov <stas@deglitch.com>
2026-03-02 21:46:25 -0800

    Add formula for Blew.

---
+++ Formula/blew.rb
@@ -0,0 +1,36 @@
+class Blew < Formula
+  desc "BLE scanner and CLI tool for macOS"
+  homepage "https://github.com/stass/blew"
+  url "https://github.com/stass/blew/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "2fd6c279b1e72a6d439be4af8b39080d8f38d6f279c6e1e17622bb7cdc2e1cb1"
+  license "BSD-2-Clause"
+
+  depends_on :macos
+  depends_on xcode: ["15.0", :build]
+
+  resource "bluetooth-numbers-database" do
+    url "https://github.com/NordicSemiconductor/bluetooth-numbers-database/archive/85317212ef563adcc57e575793a24e679c81b724.tar.gz"
+    sha256 "263b194aa90cf6ea95428b50876375785f62818d754462ef187a7ec6d40360f2"
+  end
+
+  resource "bluetooth-SIG" do
+    url "https://bitbucket.org/bluetooth-SIG/public/get/8e18798ea17a4471e093a543b48e33e72d9f211d.tar.gz"
+    sha256 "e10a707a435da83c13c655f0134d14672c6e71b01ac61668affffcde938492de"
+  end
+
+  def install
+    resource("bluetooth-numbers-database").stage do
+      (buildpath/"Vendor/bluetooth-numbers-database").install Dir["*"]
+    end
+    resource("bluetooth-SIG").stage do
+      (buildpath/"Vendor/bluetooth-SIG").install Dir["*"]
+    end
+
+    system "swift", "build", "-c", "release", "--disable-sandbox"
+    bin.install ".build/release/blew"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/blew --version")
+  end
+end
